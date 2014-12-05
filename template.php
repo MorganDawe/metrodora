5eced578ef21d2f3b59a2e696dbd04e9430ee7fc
MorganDawe <mdawe@discoverygarden.ca>
2014-12-05 16:01:24 -0400

    Fixed coder violations.

--- template.php
+++ template.php
@@ -101,130 +101,6 @@
  *   http://drupal.org/node/223440 and http://drupal.org/node/1089656
  */
 
-
-/**
- * Override or insert variables into the maintenance page template.
- *
- * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("maintenance_page" in this case.)
- */
-/* -- Delete this line if you want to use this function
-function metro_theme_preprocess_maintenance_page(&$variables, $hook) {
-  // When a variable is manipulated or added in preprocess_html or
-  // preprocess_page, that same work is probably needed for the maintenance page
-  // as well, so we can just re-use those functions to do that work here.
-  metro_theme_preprocess_html($variables, $hook);
-  metro_theme_preprocess_page($variables, $hook);
-}
-// */
-
-/**
- * Override or insert variables into the html templates.
- *
- * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("html" in this case.)
- */
-/* -- Delete this line if you want to use this function
-function metro_theme_preprocess_html(&$variables, $hook) {
-  $variables['sample_variable'] = t('Lorem ipsum.');
-
-  // The body tag's classes are controlled by the $classes_array variable. To
-  // remove a class from $classes_array, use array_diff().
-  //$variables['classes_array'] = array_diff($variables['classes_array'], array('class-to-remove'));
-}
-// */
-
-/**
- * Override or insert variables into the page templates.
- *
- * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("page" in this case.)
- */
-/* -- Delete this line if you want to use this function
-function metro_theme_preprocess_page(&$variables, $hook) {
-  $variables['sample_variable'] = t('Lorem ipsum.');
-}
-// */
-
-/**
- * Override or insert variables into the node templates.
- *
- * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("node" in this case.)
- */
-/* -- Delete this line if you want to use this function
-function metro_theme_preprocess_node(&$variables, $hook) {
-  $variables['sample_variable'] = t('Lorem ipsum.');
-
-  // Optionally, run node-type-specific preprocess functions, like
-  // metro_theme_preprocess_node_page() or metro_theme_preprocess_node_story().
-  $function = __FUNCTION__ . '_' . $variables['node']->type;
-  if (function_exists($function)) {
-    $function($variables, $hook);
-  }
-}
-// */
-
-/**
- * Override or insert variables into the comment templates.
- *
- * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("comment" in this case.)
- */
-/* -- Delete this line if you want to use this function
-function metro_theme_preprocess_comment(&$variables, $hook) {
-  $variables['sample_variable'] = t('Lorem ipsum.');
-}
-// */
-
-/**
- * Override or insert variables into the region templates.
- *
- * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("region" in this case.)
- */
-/* -- Delete this line if you want to use this function
-function metro_theme_preprocess_region(&$variables, $hook) {
-  // Don't use Zen's region--sidebar.tpl.php template for sidebars.
-  //if (strpos($variables['region'], 'sidebar_') === 0) {
-  //  $variables['theme_hook_suggestions'] = array_diff($variables['theme_hook_suggestions'], array('region__sidebar'));
-  //}
-}
-// */
-
-/**
- * Override or insert variables into the block templates.
- *
- * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("block" in this case.)
- */
-/* -- Delete this line if you want to use this function
-function metro_theme_preprocess_block(&$variables, $hook) {
-  // Add a count to all the blocks in the region.
-  // $variables['classes_array'][] = 'count-' . $variables['block_id'];
-
-  // By default, Zen will use the block--no-wrapper.tpl.php for the main
-  // content. This optional bit of code undoes that:
-  //if ($variables['block_html_id'] == 'block-system-main') {
-  //  $variables['theme_hook_suggestions'] = array_diff($variables['theme_hook_suggestions'], array('block__no_wrapper'));
-  //}
-}
-// */
-
 /**
  * Override or insert variables into the about_collection content type.
  *
@@ -321,21 +197,29 @@ function metro_theme_preprocess_views_view_fields(&$vars) {
     }
   }
 }
+
+/**
+ * Implements hook_block_view_alter().
+ *
+ * Use a metro-theme setting to alter the
+ * footer, as default or user defined.
+ */
 function metro_theme_block_view_alter(&$data, $block) {
   switch ($block->delta) {
     case 'menu-footer-menu':
-       foreach ($data['content'] as $key => $value) {
-         if (isset($data['content'][$key]['#title']) && $data['content'][$key]['#title'] === 'Logo') {
-           $path = theme_get_setting('bg_path');
-           if (variable_get('use_default_footer')) {
-             $path = drupal_get_path('theme', 'metro_theme') . "/images/icon/METRO_M-Only-Logo_2014_250x250.jpg";
-           }
-           $file_url = file_create_url($path);
-           $data['content'][$key]['#attributes']['style'] =
-             array("background-image: url($file_url);background-repeat: no-repeat;background-position: center;");
-         }
-       }
-       break;
+      foreach ($data['content'] as $key => $value) {
+        if (isset($data['content'][$key]['#title']) && $data['content'][$key]['#title'] === 'Logo') {
+          $path = theme_get_setting('bg_path');
+          if (variable_get('use_default_footer')) {
+            $path = drupal_get_path('theme', 'metro_theme') . "/images/icon/METRO_M-Only-Logo_2014_250x250.jpg";
+          }
+          $file_url = file_create_url($path);
+          $data['content'][$key]['#attributes']['style']
+            = array("background-image: url($file_url);background-repeat: no-repeat;background-position: center;");
+        }
+      }
+      break;
+
   }
 }
 
--- theme-settings.php
+++ theme-settings.php
@@ -1,19 +1,19 @@
 <?php
 /**
- * Implements hook_form_system_theme_settings_alter().
- *
- * @param $form
- *   Nested array of form elements that comprise the form.
- * @param $form_state
- *   A keyed array containing the current state of the form.
+ * @file
+ * Contains the theme's settings functions.
  */
-function metro_theme_form_system_theme_settings_alter(&$form, &$form_state, $form_id = NULL)  {
+
+/**
+ * Implements hook_form_system_theme_settings_alter().
+ */
+function metro_theme_form_system_theme_settings_alter(&$form, &$form_state, $form_id = NULL) {
   // Work-around for a core bug affecting admin themes. See issue #943212.
   if (isset($form_id)) {
     return;
   }
 
-  // Container fieldset
+  // Container fieldset.
   $form['footerbackground'] = array(
     '#type' => 'fieldset',
     '#title' => t('Footer Icon'),
@@ -26,7 +26,7 @@ function metro_theme_form_system_theme_settings_alter(&$form, &$form_state, $for
     '#default_value' => $use_default_footer_icon,
   );
 
-  // Default path for image
+  // Default path for image.
   $bg_path = theme_get_setting('bg_path');
   if (file_uri_scheme($bg_path) == 'public' && !$use_default_footer_icon) {
     $bg_path = file_uri_target($bg_path);
@@ -35,7 +35,8 @@ function metro_theme_form_system_theme_settings_alter(&$form, &$form_state, $for
     // Use the default icon otherwise.
     $bg_path = file_uri_target(drupal_get_path('theme', 'metro_theme') . "/images/icons/METRO_M-Only-Logo_2014_250x250.jpg");
   }
-  // Helpful text showing the file name, disabled to avoid the user thinking it can be used for any purpose.
+  // Helpful text showing the file name, disabled to
+  // avoid the user thinking it can be used for any purpose.
   $form['footerbackground']['bg_path'] = array(
     '#type' => 'textfield',
     '#title' => 'Path to background image',
@@ -43,14 +44,14 @@ function metro_theme_form_system_theme_settings_alter(&$form, &$form_state, $for
     '#disabled' => TRUE,
   );
 
-  // Upload field
+  // Upload field.
   $form['footerbackground']['bg_upload'] = array(
     '#type' => 'file',
     '#title' => 'Upload background image',
     '#description' => 'Upload a new image for the background.',
   );
 
-  // Attach custom submit handler to the form
+  // Attach custom submit handler to the form.
   $form['#submit'][] = 'metro_theme_settings_submit';
 
 }
@@ -65,7 +66,7 @@ function metro_theme_form_system_theme_settings_alter(&$form, &$form_state, $for
  */
 function metro_theme_settings_submit($form, &$form_state) {
   $settings = array();
-  // Get the previous value
+  // Get the previous value.
   $previous = 'public://' . $form['footerbackground']['bg_path']['#default_value'];
   variable_set('use_default_footer', $form_state['values']['default_footer']);
   $file = file_save_upload('bg_upload');
@@ -73,12 +74,12 @@ function metro_theme_settings_submit($form, &$form_state) {
     $parts = pathinfo($file->filename);
     $destination = 'public://' . $parts['basename'];
     $file->status = FILE_STATUS_PERMANENT;
-    if(file_copy($file, $destination, FILE_EXISTS_REPLACE)) {
+    if (file_copy($file, $destination, FILE_EXISTS_REPLACE)) {
       $_POST['bg_path'] = $form_state['values']['bg_path'] = $destination;
     }
   }
   else {
-    // Avoid error when the form is submitted without specifying a new image
+    // Avoid error when the form is submitted without specifying a new image.
     $_POST['bg_path'] = $form_state['values']['bg_path'] = $previous;
   }
 }
